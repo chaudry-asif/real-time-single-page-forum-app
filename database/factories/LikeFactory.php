f5d018941236b6955309605225dd220051cf71df
asif <chaudryasif2012@gmail.com>
2022-05-28 22:25:38 +0500

    factories and seed

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -7,6 +7,14 @@ use Faker\Generator as Faker;
 
 $factory->define(Like::class, function (Faker $faker) {
     return [
-        //
+
+        'reply_id'   =>  function () {
+            return \App\Model\Reply::all()->random();
+        },
+
+        'user_id'   =>  function () {
+            return \App\User::all()->random();
+        },
+
     ];
 });
